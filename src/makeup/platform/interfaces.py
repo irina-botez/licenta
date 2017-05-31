9a1c3d19f625589329b38e1826697b51f551e6d4
Irina Botez <irina.botez@eaudeweb.ro>
2017-05-31 12:18:34 +0300

    Added MUA content type

--- src/makeup/platform/interfaces.py
+++ src/makeup/platform/interfaces.py
@@ -6,9 +6,43 @@ from zope import schema
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
+from plone.supermodel import model
+from z3c.form.browser.text import TextFieldWidget
+from plone.autoform import directives
+from zope.schema import URI, Text
+
+
 
 class IMakeupPlatformLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+class IArtist(model.Schema):
+
+    title = schema.TextLine(
+        title=_(u"Name"),
+        required=True
+    )
+
+    description = schema.Text(
+        title=_(u"Description"),
+        required=False
+    )
+
+    phone = schema.TextLine(
+        title=_(u"Phone number"),
+        required=True
+    )
+
+    email = schema.TextLine(
+        title=_(u"Email"),
+        required=True
+    )
+
+    website = URI(
+        title=_(u"Website"),
+        required=False
+    )
+
 
 
 class Imakeup(Interface):
