ed913f02120e02f610bbdf0336e3f8b90f5b247d
Irina <Irina Botez>
2017-07-22 11:30:55 +0300

    Conditions for viewlets display

--- src/makeup/platform/browser/viewlets.py
+++ src/makeup/platform/browser/viewlets.py
@@ -1,7 +1,17 @@
 from plone.app.layout.viewlets import ViewletBase
 from zope.component.hooks import getSite
+from plone import api
+
 
 class MuaViewlet(ViewletBase):
+
+    def get_role(self):
+        user = api.user.get_current()
+
+        if 'Client' in user.getRoles() or api.user.is_anonymous():
+            return True
+
+        return False
 
     def get_mua_page(self):
         site = getSite()
@@ -9,6 +19,9 @@ class MuaViewlet(ViewletBase):
         return site.absolute_url() + '/@@view_mua'
 
 class RegisterViewlet(ViewletBase):
+
+    def is_anonymous(self):
+        return api.user.is_anonymous()
 
     def get_register_page(self):
         site = getSite()
