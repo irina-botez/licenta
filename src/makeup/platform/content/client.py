906da2a7a159ef5dac01dd5eed8bf26d4f83d9a1
Irina <Irina Botez>
2017-08-05 02:44:53 +0300

    Clients and MUA's add permission fix

--- src/makeup/platform/content/client.py
+++ src/makeup/platform/content/client.py
@@ -1,7 +1,7 @@
 from zope.interface import implementer
 from makeup.platform.interfaces import IClient
-from plone.dexterity.content import Container
+from plone.dexterity.content import Item
 
 @implementer(IClient)
-class Client(Container):
+class Client(Item):
     """Client Content Type"""
