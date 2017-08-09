4a5ce4e4213cfde283118db6d26efb8e25d10779
Irina <Irina Botez>
2017-08-09 23:23:54 +0300

    Uploaded images rendering on mua page

--- src/makeup/platform/browser/views.py
+++ src/makeup/platform/browser/views.py
@@ -1,4 +1,23 @@
 from Products.Five import BrowserView
+from plone import api
 
 class MuaView(BrowserView):
-    """View all makeup artists"""
+    """View a makeup artist page"""
+
+    def image_listing(self):
+        results = []
+        portal_catalog = api.portal.get_tool('portal_catalog')
+        current_path = "/".join(self.context.getPhysicalPath())
+
+        brains = portal_catalog(
+            portal_type="Image",
+            sort_on='created',
+            sort_order='descending',
+            path=current_path
+        )
+
+        for brain in brains:
+            image = brain.getObject()
+            results.append(image.absolute_url())
+
+        return results
