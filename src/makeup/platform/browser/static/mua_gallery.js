e63a6614a925fccb82a8e047ed48f90817991836
Irina <Irina Botez>
2017-08-11 19:06:01 +0300

    JS gallery on MUA page

---
+++ src/makeup/platform/browser/static/mua_gallery.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var slideIndex = 1;
+
+
+  var plusSlides = function(n) {
+      showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+      showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("img-container");
+      console.log(slides);
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+      slides[slideIndex-1].style.display = "block";
+  }
+  showSlides(slideIndex);
+
+  document.getElementById ("prev").addEventListener ("click", function() {plusSlides(-1)}, false);
+  document.getElementById ("next").addEventListener ("click", function() {plusSlides(1)}, false);
+});
