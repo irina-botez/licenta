bb3d5fc28a00dbccd2e0df6924338faabffa396d
Irina <Irina Botez>
2017-09-03 22:50:36 +0300

    Added filtering by name and rating

--- src/makeup/platform/browser/static/mua_filter.js
+++ src/makeup/platform/browser/static/mua_filter.js
@@ -1,7 +1,24 @@
 $("#search_by_fullname").on('keyup', function(){
-  console.log("HEEEI");
   var matcher = new RegExp($(this).val(), 'gi');
   $('.mua-listing').show().not(function(){
       return matcher.test($(this).find('#first-info').text())
   }).hide();
+});
+
+$('#search_by_rating').change(function(){
+    var val = $(this).val();
+    val_float = parseFloat(val) * 1.0;
+    $('.mua-rating-span').each(function(){
+      var rating_text = $(this).text().substring(0,3)
+      if(rating_text == val_float) $(this).parent().parent().show();
+      else if(val == 'any') $(this).parent().parent().show();
+      else $(this).parent().parent().hide();
+    });
+});
+
+$('#filter-btn').click(function(){
+
+  $("#search_by_fullname").val("");
+  $('#search_by_rating').val('any');
+  $('.mua-listing').show();
 });
