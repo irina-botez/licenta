d3f32dbfad5170dc9fa9c9266f0bcab88e7dae39
Irina <Irina Botez>
2017-09-06 02:07:46 +0300

    Added redirection to home page after client registration

--- src/makeup/platform/content/client.py
+++ src/makeup/platform/content/client.py
@@ -1,7 +1,13 @@
 from zope.interface import implementer
 from makeup.platform.interfaces import IClient
 from plone.dexterity.content import Item
+from zope.component.hooks import getSite
 
 @implementer(IClient)
 class Client(Item):
     """Client Content Type"""
+
+def on_add(obj, evt):
+    site = getSite()
+
+    obj.REQUEST.RESPONSE.redirect(site.absolute_url())
